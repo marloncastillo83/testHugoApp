6164bce1b26b5ff28f5942ab33337835ceadc496
Marlon Castillo Martinez <marloncastillo83@gmail.com>
2022-09-04 09:08:29 -0600

    tf files added

---
+++ instance.tf
@@ -0,0 +1,30 @@
+resource "google_compute_instance" "hugo-srv" {
+  name                      = "${local.env}-${local.project}-hugosrv"
+  machine_type              = "n1-standard-1"
+  zone                      = "${local.region}-a"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.name
+    subnetwork = google_compute_subnetwork.public.name
+
+    access_config {
+    }
+  }
+
+  tags = ["allow-web", "allow-ssh"]
+}
+
+output "hugosrv-nat-ip" {
+  value = google_compute_instance.hugo-srv.network_interface.0.access_config.0.nat_ip
+}
+
+output "hugosrv-ip" {
+  value = google_compute_instance.hugo-srv.network_interface.0.network_ip
+}
--- main.tf
+++ main.tf
@@ -1,88 +1,22 @@
 locals {
-  project = "hg-test-361420"
-  region  = "us-west2"
+  env              = "dev"
+  project          = "citizix"
+  credentials_path = "service-account.json"
+  region           = "us-west1"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">=4.20.0, < 5.0.0"
+    }
+  }
 }
 
 provider "google" {
-  credentials = file("service-account.json")
-  project     = local.project
-  region      = local.region
-}
+  credentials = file(local.credentials_path)
 
-module "vpc" {
-  source  = "terraform-google-modules/network/google//modules/subnets"
-  version = "5.2.0"
-
-  project_id   = local.project
-  network_name = "hugo-vpc"
-  routing_mode = "REGIONAL"
-
-  delete_default_internet_gateway_routes = "true"
-
-  subnets = [
-    {
-      subnet_name           = "subnet-gke"
-      subnet_ip             = "10.10.10.0/24"
-      subnet_region         = "us-west1"
-      subnet_private_access = "false"
-      subnet_flow_logs      = "true"
-      description           = "This subnet is for GKE Kubernates"
-    },
-    {
-      subnet_name           = "subnet-services"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-west1"
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-      description           = "This subnet is for Services"
-    }
-  ]
-
-  routes = [
-    {
-      name              = "egress-internet"
-      description       = "route through IGW to access internet"
-      destination_range = "0.0.0.0/0"
-      next_hop_internet = "true"
-    }
-  ]
-}
-
-module "cloud_router" {
-  source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.4"
-
-  name    = "hugo-router"
-  project = local.project
+  project = "citizix-prj"
   region  = local.region
-  network = module.vpc.network_name
-
-  nats = [{
-    name                               = "nat"
-    nat_ip_allocate_option             = "AUTO_ONLY"
-    source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-    subnetworks = [
-      {
-        name                    = "subnet-services"
-        source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-      }
-    ]
-  }]
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "hg-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initializate_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
-    }
-  }
-
-  network_interface {
-    subnetwork = "${module.vpc.subnet-services}"
-    access_config {}
-}
-
 }
